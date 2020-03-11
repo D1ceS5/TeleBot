4f7561e116f39ae87959218f503dbe49de37beaa
fateev <fateev@205-01>
2020-03-11 18:48:02 +0200

    оставили самое нужное

--- Program.cs
+++ Program.cs
@@ -20,42 +20,7 @@ namespace TeleBot
         {
             if (e.Message.Type != Telegram.Bot.Types.Enums.MessageType.Text)
                 return;
-
-            Console.WriteLine($"Msg from {e.Message.Chat.Id}");
-            switch (e.Message.Text.ToLower())
-            {
-                case "hello":
-                    client.SendTextMessageAsync(e.Message.Chat.Id, "Welocome to my chat");
-                    break;
-                case "привет":
-                    client.SendTextMessageAsync(e.Message.Chat.Id, "Добро пожаловать в чате");
-                    break;
-                case "салам":
-                    client.SendTextMessageAsync(e.Message.Chat.Id, "Валейкума!");
-                    break;
-                case "date":
-                    client.SendTextMessageAsync(e.Message.Chat.Id, DateTime.Now.ToShortDateString());
-                    break;
-                case "time":
-                    client.SendTextMessageAsync(e.Message.Chat.Id, DateTime.Now.ToShortTimeString());
-                    break;
-                case "id":
-                    client.SendTextMessageAsync(e.Message.Chat.Id, e.Message.Chat.Id.ToString());
-                    break;
-                case "btns":
-                    var markup = new ReplyKeyboardMarkup(new[]
-                    {
-                        new KeyboardButton("Привет"),
-                        new KeyboardButton("Hello"),
-                        new KeyboardButton("Салам")
-                    });
-                    markup.OneTimeKeyboard = true;
-                    client.SendTextMessageAsync(e.Message.Chat.Id, "Choose lang", replyMarkup: markup);
-                    break;
-                default:
-                    break;
-            }
-
+            client.SendTextMessageAsync(e.Message.Chat.Id, "в разработке");
         }
     }
 }
